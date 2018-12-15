e750c40fa61d96a207ca7d528d73fa5f390d12c3
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-14 19:20:09 -0800

    Add migration for active storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 ActiveRecord::Base.transaction do
   User.destroy_all
+  Business.destroy_all
 
   User.create!(email: 'demo1@gmail.com', password: 'password', f_name: 'Demo', l_name: 'User')
   User.create!(email: 'demo2@gmail.com', password: 'password', f_name: 'Demo', l_name: 'User')
@@ -18,4 +19,6 @@ ActiveRecord::Base.transaction do
   User.create!(email: 'demo8@gmail.com', password: 'password', f_name: 'Demo', l_name: 'User')
   User.create!(email: 'demo9@gmail.com', password: 'password', f_name: 'Demo', l_name: 'User')
   User.create!(email: 'demo10@gmail.com', password: 'password', f_name: 'Demo', l_name: 'User')
+
+
 end
