d7a0a90b4319d2b0f013304f023874038f469fa0
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-14 19:25:25 -0800

    Remove debugger

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -3,7 +3,6 @@ import { RECEIVE_USER, RECEIVE_SESSION_ERRORS } from "../actions/session_actions
 
 export default (state = [], action) => {
     Object.freeze(state);
-
     switch (action.type) {
         case RECEIVE_USER:
             return [];
