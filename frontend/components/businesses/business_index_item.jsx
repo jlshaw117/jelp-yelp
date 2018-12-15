54a47702a17535b1cdf8ab4cb9fe1897e445bd25
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-14 19:23:16 -0800

    Add link to business title and image to show page and index number

--- frontend/components/businesses/business_index_item.jsx
+++ frontend/components/businesses/business_index_item.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 
-const indexItem = ({ biz }) => {
+const indexItem = ({ biz, index }) => {
 
-    
+    let review = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia commodi debitis odit sed fugiat rerum dolorum omnis laudantium. Iusto dolores suscipit beatae incidunt nemo ratione quod dolore, dolorum sit facere.'
 
     return (
         <header className='business-item-container'>
-            <div className='index-item-img'></div>
+            <div className='index-item-img'><Link to={`/businesses/${biz.id}`} ><img className='business-index-img' src={biz.photo}/></Link></div>
             <div className='index-item-info'>
-                <div className='index-item-info'>
-                    <div className='index-item-info-main'></div>
-                    <div className='index-item-info-side'></div>
+                <div className='index-item-details'>
+                    <div className='index-item-info-main'>
+                        <div className='info biz-title'>{index + 1}. <Link to={`/businesses/${biz.id}`} >{biz.name}</Link></div>
+                        <div className='info biz-avg-review'>*****</div>
+                        <div className='info biz-price'>{biz.price}</div>
+                    </div>
+                    <div className='index-item-info-side'>
+                        <div className='info biz-phone'>{biz.phone_number}</div>
+                        <div className='info biz-addres'>{biz.address}</div>
+                        <div className='info biz-city'>{biz.city}, {biz.state}</div>
+                    </div>
                 </div>
-                <div className='index-item-review'></div> 
+                <div className='index-item-review'>"{review}"</div> 
             </div>
         </header>
     )
