b57612392f0eb3784130776dcc190ffd6e6f0d42
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-15 10:17:14 -0800

    Add drop down menus for home services and auto services

--- frontend/components/businesses/business_index.jsx
+++ frontend/components/businesses/business_index.jsx
@@ -17,7 +17,21 @@ class BusinessIndex extends React.Component {
             [<i class="fas fa-utensils"></i>, 'Mexican'],
             [<i class="fas fa-utensils"></i>, 'Italian'],
             [<i class="fas fa-utensils"></i>, 'Sushi']
-        ]
+        ];
+
+        const homeServices = [
+            [<i className="fas fa-home"></i>, "Contracters"],
+            [<i className="fas fa-home"></i>, 'Electricians'],
+            [<i className="fas fa-home"></i>, 'Landscaping'],
+            [<i className="fas fa-home"></i>, 'Movers']
+        ];
+
+        const autoServices = [
+            [<i className="fas fa-car"></i>, 'Auto Repair'],
+            [<i className="fas fa-car"></i>, 'Car Dealers'],
+            [<i className="fas fa-car"></i>, 'Auto Detailing'],
+            [<i className="fas fa-car"></i>, 'Towing']
+        ];
 
         let indexItems = this.props.businesses.map((biz, idx) => {
             return (
@@ -65,13 +79,18 @@ class BusinessIndex extends React.Component {
                 </header>
                 <div className='categories'>
                     <div className='categories-bar'>
-                        {/* <div className='category-item'><span>Home Services</span></div> */}
                         <div className='category-item'><CategoryDropDwon 
                             icon={<i className="fas fa-utensils category"></i>}
                             name='Restaurants'
                             subCats={restaurants}/></div>
-                        <div className='category-item'><span>Restaurants</span></div>
-                        <div className='category-item'><span>Auto Services</span></div>
+                        <div className='category-item'><CategoryDropDwon 
+                            icon={<i className="fas fa-home category"></i>}
+                            name='Home Services'
+                            subCats={homeServices}/></div>
+                        <div className='category-item'><CategoryDropDwon 
+                            icon={<i class="fas fa-car category"></i>}
+                            name='Auto Services'
+                            subCats={autoServices}/></div>
                         <div className='category-fill'></div>
                         <div className='category-item'><span>Write a Review</span></div>
                     </div>
