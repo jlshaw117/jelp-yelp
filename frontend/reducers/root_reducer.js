303bbe910c952aa18eabccba00be404f126208e6
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-14 19:24:42 -0800

    Fix import issue

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import entitiesReducer from './entities_reducer';
 import sessionReducer from './session_reducer';
-import errorsReducer from './session_errors_reducer';
+import errorsReducer from './errors_reducer';
 
 export default combineReducers({
     entities: entitiesReducer,
