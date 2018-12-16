03ad93f77831cdf6b834c2b52dab9a5402c6db8d
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-15 19:49:35 -0800

    Add current user to show container

--- frontend/components/business/business_show_container.jsx
+++ frontend/components/business/business_show_container.jsx
@@ -2,12 +2,13 @@ import { connect } from 'react-redux';
 import BusinessShow from './business_show';
 import { fetchAllBusinesses } from '../../actions/business_actions';
 
-const mapStateToProps = ({ entities }, ownProps) => {
+const mapStateToProps = ({ entities, session }, ownProps) => {
 
     let id = ownProps.match.params.businessId;
     let biz = entities.businesses[id];
     // debugger
     return ({
+        currentUser: entities.users[session.id],
         biz
     });
 };
