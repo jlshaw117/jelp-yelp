a385e6e39ccc42ea4b285045382908b44acbe326
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-15 19:49:59 -0800

    Add const session links and const greetings

--- frontend/components/business/business_show.jsx
+++ frontend/components/business/business_show.jsx
@@ -10,6 +10,29 @@ class BusinessShow extends React.Component {
 
     render () {
 
+        const sessionLinks = () => {
+
+            return (
+                <ul className='index-session-links'>
+                    <li className='log'><Link className='index-login' to='/login'>Log In</Link></li>
+                    <li className='sign'><Link className='index-signup' to='/signup'>Sign Up</Link></li>
+                </ul>
+            );
+        }
+
+        const greeting = () => {
+
+            return (
+                <div className='index-session-links'>
+                    <div className='index-sprites'>
+                        <i className="fas fa-comment-alt fa-lg"></i>
+                        <i className="fas fa-bell fa-lg"></i>
+                    </div>
+                    <DropDownMenuContainer />
+                </div>
+            );
+        };
+        
         return (
             <h1>{this.props.biz.name}</h1>
         )
