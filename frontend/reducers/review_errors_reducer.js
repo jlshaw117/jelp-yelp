5c43087f37aadf5eaf29ed87f3689dbb591a4478
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-16 18:36:27 -0800

    Add review errors reducer

---
+++ frontend/reducers/review_errors_reducer.js
@@ -0,0 +1,15 @@
+import { merge } from 'lodash';
+import { RECIEVE_REVIEW_ERRORS, RECIEVE_REVIEW } from '../actions/review_actions';
+
+export default (state = [], action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECIEVE_REVIEW_ERRORS:
+            return action.errors;
+        case RECIEVE_REVIEW:
+            return null;
+        default:
+            return state;
+    }
+};
