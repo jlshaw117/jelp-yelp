93a12d9c5511a2143065aa198d3f5bfccb60e7db
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-16 20:01:26 -0800

    Add edit review form container

---
+++ frontend/components/reviews/edit_review_form_container.jsx
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import ReviewForm from './review_form';
+import { updateReview } from '../../actions/review_actions';
+
+const mapStateToProps = ({ session, entities }, ownProps) => {
+
+    let bizId = ownProps.match.params.businessId;
+    let reviewId = ownProps.match.params.reviewId;
+    let review = entities[reviewId];
+    return ({
+        currentUser: entities.users[session.id],
+        formType: 'Edit a Review',
+        review
+    });
+};
+
+const mapDispatchToProps = dispatch => {
+
+    return ({
+        updateReview: (id) => dispatch(updateReview(id))
+    });
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewForm);
