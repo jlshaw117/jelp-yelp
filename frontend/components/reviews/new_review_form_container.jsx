72c408b19152c6f3cf44a898a0047bdc4d296694
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-16 18:53:39 -0800

    Add container for a new review form

---
+++ frontend/components/reviews/new_review_form_container.jsx
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import ReviewForm from './review_form';
+import { createReview } from '../../actions/review_actions';
+
+const mapStateToProps = ({ session, entities }, ownProps) => {
+
+    let id = ownProps.match.params.businessId;
+    return ({
+        currentUser: entities.users[session.id],
+        formType: 'Write a Review',
+        review: {
+            rating: null,
+            body: null,
+            user_id: session.id,
+            business_id: id
+        }
+    });
+};
+
+const mapDispatchToProps = dispatch => {
+
+    return ({
+        createReview: () => dispatch(createReview())
+    });
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewForm);
