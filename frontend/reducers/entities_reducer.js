5bd448282387c18763b48f4969b79f1a00479551
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-16 18:35:53 -0800

    Add reviews to entities slice of state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import businessesReducer from './businesses_reducer';
+import reviewsReducer from './review_reducer';
 
 
 export default combineReducers({
     users: usersReducer,
-    businesses: businessesReducer
+    businesses: businessesReducer,
+    reviews: reviewsReducer
 });
