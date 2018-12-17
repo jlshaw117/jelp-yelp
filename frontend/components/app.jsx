2f4c52930b2b46f99dad3791c5f860b0a754d07a
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-16 20:00:44 -0800

    Add routes for new and edit review form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,6 +6,8 @@ import LoginFormContainer from './session_form/login_form_container';
 import BusinessIndexContainer from './businesses/business_index_container';
 import { AuthRoute } from '../util/route_util';
 import BusinessShowContainer from './business/business_show_container';
+import NewReviewFormContainer from './reviews/new_review_form_container';
+import EditReviewFormContainer from './reviews/edit_review_form_container';
 
 const App = () => {
 
@@ -18,6 +20,8 @@ const App = () => {
             <Route exact path="/" component={GreetingContainer} />
             <Route exact path="/businesses" component={BusinessIndexContainer} />
             <Route exact path="/businesses/:businessId" component={BusinessShowContainer} />
+            <Route exact path="/businesses/:businessId/newReview" component={NewReviewFormContainer} />
+            <Route exact path="/businesses/:businessId/editReview/:reviewId" component={EditReviewFormContainer} />
             <AuthRoute path="/login" component={LoginFormContainer} />
             <AuthRoute path="/signup" component={SignupFormContainer} />
         </div>
