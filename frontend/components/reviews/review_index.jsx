87d1eea58e743a520871d6c305acaad5d5c7f1f3
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-17 21:20:21 -0800

    Add a review index

---
+++ frontend/components/reviews/review_index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default (props) => {
+
+    const indexItems = props.reviews.map((review) => {
+
+        const rating = () => {
+            switch (review.rating) {
+                case 5:
+                    return 'five';
+                case 4:
+                    return 'four';
+                case 3:
+                    return 'three';
+                case 2:
+                    return 'two';
+                case 1:
+                    return 'one';
+                default:
+                    break;
+            }
+        };
+
+        return (
+            <div className='review-index-item'>
+                <div className='biz-show-review-left'>
+                    <div className='biz-show-user-profile-img'><Link to='/'><img src={window.dog} /></Link></div>
+                    <div className='biz-show-user-details'>
+                        <div className='biz-show-user-name'><Link to='/'>Demo U.</Link></div>
+                    </div>
+                </div>
+                <div className='review-index-item-right'>
+                    <div id={rating()} className='small-star-rating'></div>
+                    <p className='review-index-body'>{review.body}</p>
+                </div>
+            </div>
+        )
+    });
+    return (
+        <div className='review-index'>
+            {indexItems}
+        </div>
+    )
+};
