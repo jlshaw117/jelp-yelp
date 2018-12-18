677c90ddf5d8f6f75691bdd198647ebf4efa2ef6
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-17 17:41:26 -0800

    Update review reducer to recieve business

--- frontend/reducers/review_reducer.js
+++ frontend/reducers/review_reducer.js
@@ -1,5 +1,6 @@
 import { merge } from 'lodash';
 import { RECIEVE_REVIEW, REMOVE_REVIEW } from '../actions/review_actions';
+import { RECEIVE_BUSINESS } from '../actions/business_actions';
 
 export default (state = {}, action) => {
     Object.freeze(state);
@@ -7,11 +8,17 @@ export default (state = {}, action) => {
     let newState = merge({}, state);
     switch (action.type) {
         case RECIEVE_REVIEW:
-            newState[action.review.id] = action.review;
+            // newState[action.review.id] = action.review;
+            // return newState;
+            const { review, average_rating } = action;
+            newState[review.business_id].reviewIds.push(review.id);
+            newState[review.business_id].average_rating = average_rating;
             return newState;
         case REMOVE_REVIEW:
             delete newState[action.review.id];
             return newState;
+        case RECEIVE_BUSINESS:
+            return action.reviews;
         default:
             return state;
     }
