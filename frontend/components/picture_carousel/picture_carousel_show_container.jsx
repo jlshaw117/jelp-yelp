ccff71f0d2e2eebd7ea408a3f99d7939dfd8b20c
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-18 09:56:37 -0800

    Creat skeleton for picture carousel container

---
+++ frontend/components/picture_carousel/picture_carousel_show_container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import PictureCarouselShow from './picture_carousel_show';
+
+const mapStateToProps = state => {
+
+    return ({
+
+    });
+};
+
+const mapDispatchToProps = dispatch => {
+
+    return ({
+
+    });
+};
+
+export default connect (mapStateToProps, mapDispatchToProps)(PictureCarouselShow);
