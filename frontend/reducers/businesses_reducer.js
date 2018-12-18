0d37a0704673ec835013d73cea16904c3eefb505
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-17 17:41:04 -0800

    Clean out some comments

--- frontend/reducers/businesses_reducer.js
+++ frontend/reducers/businesses_reducer.js
@@ -5,10 +5,8 @@ export default (state = {}, action) => {
     Object.freeze(state);
 
     let newState = merge({}, state)
-    // debugger
     switch (action.type) {
         case RECEIVE_BUSINESS:
-            // newState = merge({}, state, { [action.business.id]: action.business});
             newState[action.business.id] = action.business;
             return newState;
         case RECEIVE_ALL_BUSINESSES:
