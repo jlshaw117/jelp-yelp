0d597935fbdabfdded00b8ef5a2a1864e98d85f3
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-18 09:56:15 -0800

    Create skeleton for picture carousel componenet

---
+++ frontend/components/picture_carousel/picture_carousel_show.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class PictureCarouselShow extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    render () {
+
+        return (
+            
+        )
+    }
+}
+
+export default PictureCarouselShow;
