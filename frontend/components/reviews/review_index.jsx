4db6dbbbd0c15dcdc68d2ce4363d9fcea8b74c5a
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-18 16:15:14 -0800

    Use user info

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -25,9 +25,9 @@ export default (props) => {
         return (
             <div key={idx} className='review-index-item'>
                 <div className='biz-show-review-left'>
-                    <div className='biz-show-user-profile-img'><Link to='/'><img src={window.dog} /></Link></div>
+                    <div className='biz-show-user-profile-img'><Link to='/'><img className='user-profile-image' src={props.users[review.user_id].photo} /></Link></div>
                     <div className='biz-show-user-details'>
-                        <div className='biz-show-user-name'><Link to='/'>Demo U.</Link></div>
+                        <div className='biz-show-user-name'><Link to='/'>{props.users[review.user_id].f_name} {props.users[review.user_id].l_name[0]}.</Link></div>
                     </div>
                 </div>
                 <div className='review-index-item-right'>
