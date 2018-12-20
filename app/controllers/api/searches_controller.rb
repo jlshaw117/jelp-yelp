b7a42ca7fda72dd67f131d79f832df827b1aaff3
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-19 21:15:35 -0800

    Add searches controller

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,10 +1,11 @@
 class Api::SearchesController < ApplicationController
 
     def search_businesses
-        
-    end
-
-    def search_params
-        params.require(:search).permit(:terms)
+        search_terms = params[:terms].split
+        result_string = search_terms.map do |term|
+            "LOWER(name) LIKE '%#{term.downcase}%'"
+        end.join(" OR ")
+        @businesses = Business.where(result_string)
+        render 'api/businesses/index'
     end
 end
