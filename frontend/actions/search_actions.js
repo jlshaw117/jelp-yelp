1fe06c8b7957949c1a99fa373b3d0837f6245a9b
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-21 11:01:19 -0800

    Remove edit out code

--- frontend/actions/search_actions.js
+++ frontend/actions/search_actions.js
@@ -1,5 +1,4 @@
 import * as SearchAPIUtils from '../util/search_api_util';
-// import {receiveAllBusinesses} from './business_actions';
 
 export const RECIEVE_BUSINESS_RESULTS = 'RECIEVE_BUSINESS_RESULTS';
 
