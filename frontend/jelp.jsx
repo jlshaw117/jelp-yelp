9dc518033ef44d4a1c5b74af139df9faa4646081
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-21 11:01:04 -0800

    Remove functions testing functions from window and logger

--- frontend/jelp.jsx
+++ frontend/jelp.jsx
@@ -2,9 +2,6 @@ import React from  'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { login } from './actions/session_actions';
-import { createReview, deleteReview, updateReview } from './actions/review_actions';
-import { searchBusinesses, fetchAllBusinesses } from './actions/business_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -22,18 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         store = configureStore();
     }
-    // let store = configureStore();
     ReactDOM.render(<Root store={store}/>, root);
 
-    //testing
-    window.searchBusinesses = searchBusinesses;
-    window.fetchAllBusinesses = fetchAllBusinesses;
-    window.createReview = createReview;
-    window.deleteReview = deleteReview;
-    window.updateReview = updateReview;
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.login = login;
-    //end testing
 });
 
