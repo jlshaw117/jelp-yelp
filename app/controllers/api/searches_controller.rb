4b373a2a1c540f53e305c2be9b265fb44679f668
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-21 08:21:00 -0800

    Refactor search controller

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,11 +1,13 @@
 class Api::SearchesController < ApplicationController
 
     def search_businesses
-        search_terms = params[:terms].split
+        @string = params[:terms]
+        search_terms = @string.split
         result_string = search_terms.map do |term|
             "LOWER(name) LIKE '%#{term.downcase}%'"
         end.join(" OR ")
-        @businesses = Business.where(result_string)
-        render 'api/businesses/index'
+        @result = Business.where(result_string)
+        @results = @result.empty? ? false : true
+        render 'api/searches/business_results'
     end
 end
