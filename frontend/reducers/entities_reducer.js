9af65f86136ebfa8615f6aeb5f6a691887f720b0
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-21 08:24:58 -0800

    Add search reducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import businessesReducer from './businesses_reducer';
 import reviewsReducer from './review_reducer';
+import searchReducer from './search_reducer';
 
 
 export default combineReducers({
     users: usersReducer,
     businesses: businessesReducer,
-    reviews: reviewsReducer
+    reviews: reviewsReducer,
+    search: searchReducer
 });
