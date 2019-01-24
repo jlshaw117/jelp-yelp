b84cc123a4dd62ae635344e8b941e37b8b7d9181
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-24 09:43:04 -0800

    Add nav links to footer to social media sites

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 
 class Footer extends React.Component {
 
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <footer>
+                <div className='contact-info'>
+                    <div className='footer-info'>
+                        Get in touch with the developer
+                    </div>
+                    <nav className='footer-links'>
+                        <a href="https://www.linkedin.com/in/jerrik-shaw/"><i class="fab fa-linkedin"></i></a>
+                        <a href="https://angel.co/jerrik-shaw?al_content=view+your+profile&al_source=transaction_feed%2Fnetwork_sidebar"><i class="fab fa-angellist"></i></a>
+                        <a href="https://www.jerrikshaw.com/"><i class="fas fa-briefcase"></i></a>
+                        <a href="mailto: jlshaw117@gmail.com"><i class="fas fa-envelope"></i></a>
+                    </nav>
+                </div>
+                <div className='footer-img'></div>
+            </footer>
+        );
+    }
 }
 
 export default Footer;
