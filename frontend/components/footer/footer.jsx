386122aa1664f1efbb13ba9d74b3310066f86013
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-24 10:53:30 -0800

    Add class names to icons for styling

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -16,10 +16,10 @@ class Footer extends React.Component {
                             Get in touch with the developer
                         </div>
                         <nav className='footer-links'>
-                            <a href="https://www.linkedin.com/in/jerrik-shaw/"><i className="fab fa-linkedin"></i></a>
-                            <a href="https://angel.co/jerrik-shaw?al_content=view+your+profile&al_source=transaction_feed%2Fnetwork_sidebar"><i className="fab fa-angellist"></i></a>
-                            <a href="https://www.jerrikshaw.com/"><i className="fas fa-briefcase"></i></a>
-                            <a href="mailto: jlshaw117@gmail.com"><i className="fas fa-envelope"></i></a>
+                            <a className='email' href="mailto: jlshaw117@gmail.com"><i className="fas fa-envelope"></i></a>
+                            <a className='portfolio' href="https://www.jerrikshaw.com/"><i className="fas fa-briefcase"></i></a>
+                            <a className='linkedin' href="https://www.linkedin.com/in/jerrik-shaw/"><i className="fab fa-linkedin"></i></a>
+                            <a className='angellist' href="https://angel.co/jerrik-shaw?al_content=view+your+profile&al_source=transaction_feed%2Fnetwork_sidebar"><i className="fab fa-angellist"></i></a>
                         </nav>
                     </div>
                     <div className='footer-img'></div>
