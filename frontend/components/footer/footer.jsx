cecb7928c006fbb8e149a4b381fc1571f37392e3
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-24 10:28:14 -0800

    Add inner wrapper

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -10,18 +10,20 @@ class Footer extends React.Component {
 
         return (
             <footer>
-                <div className='contact-info'>
-                    <div className='footer-info'>
-                        Get in touch with the developer
+                <div className='footer-inner-wrapper'>
+                    <div className='contact-info'>
+                        <div className='footer-info'>
+                            Get in touch with the developer
+                        </div>
+                        <nav className='footer-links'>
+                            <a href="https://www.linkedin.com/in/jerrik-shaw/"><i className="fab fa-linkedin"></i></a>
+                            <a href="https://angel.co/jerrik-shaw?al_content=view+your+profile&al_source=transaction_feed%2Fnetwork_sidebar"><i className="fab fa-angellist"></i></a>
+                            <a href="https://www.jerrikshaw.com/"><i className="fas fa-briefcase"></i></a>
+                            <a href="mailto: jlshaw117@gmail.com"><i className="fas fa-envelope"></i></a>
+                        </nav>
                     </div>
-                    <nav className='footer-links'>
-                        <a href="https://www.linkedin.com/in/jerrik-shaw/"><i className="fab fa-linkedin"></i></a>
-                        <a href="https://angel.co/jerrik-shaw?al_content=view+your+profile&al_source=transaction_feed%2Fnetwork_sidebar"><i className="fab fa-angellist"></i></a>
-                        <a href="https://www.jerrikshaw.com/"><i className="fas fa-briefcase"></i></a>
-                        <a href="mailto: jlshaw117@gmail.com"><i className="fas fa-envelope"></i></a>
-                    </nav>
+                    <div className='footer-img'></div>
                 </div>
-                <div className='footer-img'></div>
             </footer>
         );
     }
